789bafce1d34a1c2db46dbde1f5b83f347c11156
sericakitty <63978546+sericakitty@users.noreply.github.com>
2023-10-12 03:07:19 +0300

    add file

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+// importing footer component
+const footer = () => {
+  document.getElementsByTagName('footer')[0].innerHTML = `© 2020 - ${new Date().getFullYear()} Serica Kitty. All Rights Reserved.`
+}
+
+footer()
+
+
